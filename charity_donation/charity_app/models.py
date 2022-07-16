e757a91f7c69b4dad1f0f25c4fce96c939942ab9
Sowa09 <tsowa09@gmail.com>
2022-07-16 15:37:19 +0200

    donation model added

--- charity_donation/charity_app/models.py
+++ charity_donation/charity_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.forms import ModelForm
 
 FUND_TYPE = [
     ('fundacja', 'fundacja'),
@@ -16,4 +18,18 @@ class Institution(models.Model):
     description = models.TextField()
     type = models.CharField(max_length=63, choices=FUND_TYPE, default=FUND_TYPE[0][0])
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=32)
+    zip_code = models.IntegerField()
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
 
