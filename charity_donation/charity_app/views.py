69a9b995e1a7c4fa3d9f9e5202209e06e44d97d2
Sowa09 <tsowa09@gmail.com>
2022-07-16 21:11:54 +0200

    views updated

--- charity_donation/charity_app/views.py
+++ charity_donation/charity_app/views.py
@@ -1,9 +1,14 @@
 from django.db.models.aggregates import Sum
-from django.db.models.expressions import F
 from django.shortcuts import render
 from django.views import View
 
 from .models import Donation, Institution
+
+
+class TestView(View):
+
+    def get(self, request):
+        return render(request, 'base.html')
 
 
 class AddDonationView(View):
@@ -29,7 +34,10 @@ class IndexView(View):
     def get(self, request):
         donation_counter = Donation.objects.aggregate(TOTAL=Sum('quantity'))['TOTAL']
         institution_counter = Donation.objects.values('institution').distinct().count()
+        institution = Institution.objects.all()
 
         ctx = {'donation_counter': donation_counter,
-               'institution_counter': institution_counter}
+               'institution_counter': institution_counter,
+               'institution': institution,
+               }
         return render(request, 'index.html', ctx)
