9e2cb44f4b13808b9602fe2583fce9e9206ede94
Sowa09 <tsowa09@gmail.com>
2022-07-16 15:19:12 +0200

    institution model added

--- charity_donation/charity_app/models.py
+++ charity_donation/charity_app/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
+FUND_TYPE = [
+    ('fundacja', 'fundacja'),
+    ('organizacja pozarządowa', 'organizacja pozarządowa'),
+    ('zbiórka lokalna', 'zbiórka lokalna'),
+]
+
 
 class Category(models.Model):
     name = models.CharField(max_length=32)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+    type = models.CharField(max_length=63, choices=FUND_TYPE, default=FUND_TYPE[0][0])
+    categories = models.ManyToManyField(Category)
+
